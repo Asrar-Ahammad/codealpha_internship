3715bf51ec6dd6226bc93b67be2bab33dd83f156
Asrar-Ahammad <asrarahammadshaik@gmail.com>
2023-10-31 14:40:00 +0530

    second commit

--- image_recognition_task_3/src/app.py
+++ image_recognition_task_3/src/app.py
@@ -4,6 +4,7 @@ face_classifier = cv2.CascadeClassifier(
     cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
 )
 
+
 video_capture = cv2.VideoCapture(0)
 
 def detect_bounding_box(vid):
